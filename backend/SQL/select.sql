c3f1730b7c5e2ef113829017d30249bd22b995f0
ThibaultFr94 <thibault.fromageau@gmail.com>
2023-09-06 09:13:56 +0200

    create select

---
+++ backend/SQL/select.sql
@@ -0,0 +1,15 @@
+-- Sélectionner tous les utilisateurs
+
+SELECT * FROM artsphereai.User;
+
+-- Sélectionner tous les types d'art
+
+SELECT * FROM artsphereai.ArtType;
+
+-- Sélectionner tous les types d'art favoris des utilisateurs
+
+SELECT * FROM artsphereai.UserFavoriteType;
+
+-- Sélectionner toutes les œuvres d'art
+
+SELECT * FROM artsphereai.Art;
