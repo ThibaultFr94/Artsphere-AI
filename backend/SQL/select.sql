44e29039d08e6f79fc7f4f64256d5fa31e73e661
ThibaultFr94 <thibault.fromageau@gmail.com>
2023-09-13 15:25:27 +0200

    sql changes

--- backend/SQL/select.sql
+++ backend/SQL/select.sql
@@ -1,15 +1,7 @@
--- Sélectionner tous les utilisateurs
-
 SELECT user.* FROM artsphereai.user;
-
--- Sélectionner tous les types d'art
 
 SELECT type.* FROM artsphereai.type;
 
--- Sélectionner tous les types d'art favoris des utilisateurs
-
 SELECT user_type.* FROM artsphereai.user_type;
-
--- Sélectionner toutes les œuvres d'art
 
 SELECT art.* FROM artsphereai.art;
