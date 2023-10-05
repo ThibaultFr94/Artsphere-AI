539bd69c4c0e1266536866f8674c0e083a4b02fc
ThibaultFr94 <thibault.fromageau@gmail.com>
2023-10-05 15:10:42 +0200

    update gitignore

--- src/frontend/asset/js/module_env.js
+++
@@ -1,6 +0,0 @@
-export const OPENAI_TEXT_API_KEY =
-  "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";
-export const OPENAI_IMAGE_API_KEY =
-  "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";
-export const OPENAI_TEXT_URL = "https://api.openai.com/v1/chat/completions";
-export const OPENAI_IMAGE_URL = "https://api.openai.com/v1/images/generations";
