e122a0ce68dbdde1a633c642209f5cb3b616fb91
ThibaultFr94 <thibault.fromageau@gmail.com>
2023-10-05 10:42:41 +0200

    api key

--- src/frontend/asset/js/module_env.js
+++ src/frontend/asset/js/module_env.js
@@ -1,6 +1,6 @@
 export const OPENAI_TEXT_API_KEY =
-  "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";
+  "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";
 export const OPENAI_IMAGE_API_KEY =
-  "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";
+  "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";
 export const OPENAI_TEXT_URL = "https://api.openai.com/v1/chat/completions";
 export const OPENAI_IMAGE_URL = "https://api.openai.com/v1/images/generations";
--- src/frontend/asset/js/service.js
+++ src/frontend/asset/js/service.js
@@ -16,20 +16,12 @@ export function shuffle(arr) {
     .map(([, i]) => arr[i]);
 }
 
-export function shuffleNorman(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
 export function generatePromptGraff(graff) {
   return `The subject is a graffiti. It's bold, intricate, colorfull. The medium is spray paint, with techniques like stenciling and dripping adding texture and depth. Some artists who inspire this style are Banksy and Shepard Fairey, switch each time, based on this name : ${graff}`;
 }
 
 export function generatePromptPaint(paint) {
-  return `The subject will be a painting art. a beautiful landscape, amazing color ,using the style of Edouard Manet,  Rembrandt Harmenszoon van Rijn.  ${paint} is his name`;
+  return `The subject will be includes a variety of painting art, beautiful landscape, amazing color ,using the style of Edouard Manet, Rembrandt Harmenszoon van Rijn.  ${paint} is his name`;
 }
 
 export function generatePromptInt(int) {
